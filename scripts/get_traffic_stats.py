57fddec88c59a9b7fad5d0efc8e8d66550045aa7
Maria Sharabayko <msharabayko@haivision.com>
2021-07-23 18:16:14 +0200

    Saved intermediate work

--- scripts/get_traffic_stats.py
+++ scripts/get_traffic_stats.py
@@ -10,41 +10,39 @@ import tcpdump_processing.convert as convert
 import tcpdump_processing.extract_packets as extract_packets
 
 
-class SRTTrafficStatsIndex:
+class TrafficStatsIndex:
 	def __init__(self, srt_packets):
 		self.ctrl_pkts        = (srt_packets['srt.iscontrol'] == 1)
 		self.ctrl_pkts_ack    = (self.ctrl_pkts) & (srt_packets['srt.type'] == '0x00000002')
 		self.ctrl_pkts_ackack = (self.ctrl_pkts) & (srt_packets['srt.type'] == '0x00000006')
 		self.ctrl_pkts_nak    = (self.ctrl_pkts) & (srt_packets['srt.type'] == '0x00000003')
 
-		self.data_pkts = (srt_packets['srt.iscontrol'] == 0)
-		self.data_pkts_org = self.data_pkts & (srt_packets['srt.msg.rexmit'] == 0)
-		self.data_pkts_rex = self.data_pkts & (srt_packets['srt.msg.rexmit'] == 1)
-
+		self.data_pkts     = (srt_packets['srt.iscontrol'] == 0)
+		self.data_pkts_org = (self.data_pkts) & (srt_packets['srt.msg.rexmit'] == 0)
+		self.data_pkts_rex = (self.data_pkts) & (srt_packets['srt.msg.rexmit'] == 1)
 
 
 class TrafficStats:
 	def __init__(self, srt_packets):
 		self.srt_packets = srt_packets
-		self.index = SRTTrafficStatsIndex(srt_packets)
+		self.index = TrafficStatsIndex(srt_packets)
 		self.time_start = srt_packets.iloc[ 0]['ws.time']
 		self.time_stop  = srt_packets.iloc[-1]['ws.time']
 		self.duration_sec  = self.time_start - self.time_stop
-		return
 
 	def bytes_to_Mbps(self, bytes):
 		return bytes * 8 / self.duration_sec / 1000000
 
 	@property
-	def srt_pkts_data(self):
+	def srt_data_pkts(self):
 		return self.srt_packets[self.index.data_pkts]
 
 	@property
-	def srt_pkts_data_org(self):
+	def srt_data_pkts_org(self):
 		return self.srt_packets[self.index.data_pkts_org]
 
 	@property
-	def srt_pkts_data_rex(self):
+	def srt_data_pkts_rex(self):
 		return self.srt_packets[self.index.data_pkts_rex]
 
 	@property
@@ -52,49 +50,53 @@ class TrafficStats:
 		return self.srt_packets[self.index.ctrl_pkts]
 
 	def generate_report(self):
-		srt_pkts_cnt           = len(self.srt_packets.index)
-		srt_data_pkts_cnt      = self.index.data_pkts.sum()	    # count true values
-		srt_data_pkts_org_cnt  = self.index.data_pkts_org.sum() # count true values
-		srt_data_pkts_rex_cnt  = self.index.data_pkts_rex.sum() # count true values
+		# Count the number of packets.
+		srt_pkts_cnt          = len(self.srt_packets.index)
+		srt_data_pkts_cnt     = self.index.data_pkts.sum()
+		srt_data_pkts_org_cnt = self.index.data_pkts_org.sum()
+		srt_data_pkts_rex_cnt = self.index.data_pkts_rex.sum()
 
-		srt_ctrl_pkts_cnt = self.index.ctrl_pkts.sum()
-		srt_ctrl_pkts_ack_cnt = self.index.ctrl_pkts_ack.sum()
+		srt_ctrl_pkts_cnt        = self.index.ctrl_pkts.sum()
+		srt_ctrl_pkts_ack_cnt    = self.index.ctrl_pkts_ack.sum()
 		srt_ctrl_pkts_ackack_cnt = self.index.ctrl_pkts_ackack.sum()
-		srt_ctrl_pkts_nak_cnt = self.index.ctrl_pkts_nak.sum()
+		srt_ctrl_pkts_nak_cnt    = self.index.ctrl_pkts_nak.sum()
 
-		# Calculating lost packets as the number of original packets that
-		# haven't reached the receiver. Reordering is taken into account,
-		# so if a packet is reordered and comes later, it will not be
-		# considered as lost
-		seqnos_org = self.srt_pkts_data_org['srt.seqno'].astype('int32')
-		# Removing duplicates in received original packets
+		# Calculate the number of lost original data packets as the number
+		# of original data packets that haven't reached the receiver.
+		# Reordering is taken into account, so if a packet is reordered and
+		# comes later, it will not be considered as lost.
+		seqnos_org = self.srt_data_pkts_org['srt.seqno'].astype('int32')
+		# Removing duplicates in received original packets.
 		seqnos_org = seqnos_org.drop_duplicates()
-		srt_pkts_data_org_lost = int((seqnos_org.diff() - 1).sum())
+		srt_data_pkts_org_lost = int((seqnos_org.diff().dropna() - 1).sum())
 
 		# The number of packets considered missing at the receiver.
 		# It means nor original, neither retransmitted packet with
 		# a particular sequence number hasn't reached the destination
-		seqnos = self.srt_pkts_data['srt.seqno'].astype('int32').copy()
+		seqnos = self.srt_data_pkts['srt.seqno'].astype('int32').copy()
 		seqnos = seqnos.drop_duplicates().sort_values()
-		srt_data_pkts_missing = int((seqnos.diff() - 1).sum())
+		srt_data_pkts_missing = int((seqnos.diff().dropna() - 1).sum())
 
-		rexmit_pkts                   = self.srt_pkts_data_rex.copy()
-		rexmit_pkts['srt.seqno']      = rexmit_pkts['srt.seqno'].astype('int32')
-		rexmit_pkts['seqno']          = rexmit_pkts['srt.seqno']
-		srt_data_rexmits              = rexmit_pkts.groupby(['srt.seqno'])['seqno'].count()
-		srt_data_rex_once_cnt         = srt_data_rexmits[srt_data_rexmits == 1].count()
-		srt_data_rex_twice_cnt        = srt_data_rexmits[srt_data_rexmits == 2].count()
-		srt_data_rex_3x_cnt           = srt_data_rexmits[srt_data_rexmits == 3].count()
-		srt_data_rex_4x_cnt           = srt_data_rexmits[srt_data_rexmits == 4].count()
-		srt_data_rex_5x_more_cnt      = srt_data_rexmits[srt_data_rexmits > 4].count()
+		# Calculate how much packets were retransmitted once, twice, 3x times, etc.
+		# If there was no dropped packets, rexmits = the number of recovered packets
+		rexmit_pkts              = self.srt_data_pkts_rex.copy()
+		rexmit_pkts['srt.seqno'] = rexmit_pkts['srt.seqno'].astype('int32')
+		rexmit_pkts['seqno']     = rexmit_pkts['srt.seqno']
+		rexmits                  = rexmit_pkts.groupby(['srt.seqno'])['seqno'].count()
+		rex_once_cnt             = rexmits[rexmits == 1].count()
+		rex_twice_cnt            = rexmits[rexmits == 2].count()
+		rex_3x_cnt               = rexmits[rexmits == 3].count()
+		rex_4x_cnt               = rexmits[rexmits == 4].count()
+		rex_5x_more_cnt          = rexmits[rexmits > 4].count()
 
 		def to_percent(value, base):
 			return round(value / base * 100, 2)
 
-		data_pkts_org_received_lost = srt_data_pkts_org_cnt + srt_pkts_data_org_lost
+		data_pkts_org_received_lost = srt_data_pkts_org_cnt + srt_data_pkts_org_lost
 
 		print(" SRT Packets ".center(70, "~"))
-		
+
+		print(f"- SRT pkts                        {srt_pkts_cnt:>26}")		
 		print(f"- SRT DATA pkts                   {srt_data_pkts_cnt:>26}")
 		print(
 			f"  - Original DATA pkts received   {srt_data_pkts_org_cnt:>26}"
@@ -102,15 +104,16 @@ class TrafficStats:
 			"  out of SRT DATA pkts"
 		)
 		print(
-			f"  - Original DATA pkts lost       {srt_pkts_data_org_lost:>26}"
-			f" {to_percent(srt_pkts_data_org_lost, data_pkts_org_received_lost):>8}%"
+			f"  - Original DATA pkts lost       {srt_data_pkts_org_lost:>26}"
+			f" {to_percent(srt_data_pkts_org_lost, data_pkts_org_received_lost):>8}%"
 			"  out of original DATA pkts (received+lost)"
 		)
 		print(
-			f"  - DATA pkts retransmitted       {srt_data_pkts_rex_cnt:>26}"
+			f"  - Retransmitted DATA pkts received       {srt_data_pkts_rex_cnt:>26}"
 			f" {to_percent(srt_data_pkts_rex_cnt, srt_data_pkts_cnt):>8}%"
 			"  out of SRT DATA pkts"
 		)
+		print("DATA packets recovered")
 		print(
 			f"  - DATA pkts missing             {srt_data_pkts_missing:>26}"
 			f" {to_percent(srt_data_pkts_missing, data_pkts_org_received_lost):>8}%"
@@ -122,17 +125,21 @@ class TrafficStats:
 		print(f"  - ACKACK pkts received          {srt_ctrl_pkts_ackack_cnt:>26}")
 		print(f"  - NAK pkts sent                 {srt_ctrl_pkts_nak_cnt:>26}")
 
-		print(
-			f"- Recovered pkts (lost-missing)   {srt_data_rexmits.count():>26}"
-			f" {to_percent(srt_data_rexmits.count(), data_pkts_org_received_lost):>8}%"
-			"  out of original DATA pkts (received+lost)"
-		)
+		# print(
+		# 	f"- Recovered pkts (lost-missing)   {rexmits.count():>26}"
+		# 	f" {to_percent(rexmits.count(), data_pkts_org_received_lost):>8}%"
+		# 	"  out of original DATA pkts (received+lost)"
+		# )
+
+		# TODO Check the delimeter
 		print("  Retransmitted")
-		print(f"     once:                        {srt_data_rex_once_cnt:>26} {to_percent(srt_data_rex_once_cnt, data_pkts_org_received_lost):>8}%")
-		print(f"     twice:                       {srt_data_rex_twice_cnt:>26} {to_percent(srt_data_rex_twice_cnt, data_pkts_org_received_lost):>8}%")
-		print(f"     3×:                          {srt_data_rex_3x_cnt:>26} {to_percent(srt_data_rex_3x_cnt, data_pkts_org_received_lost):>8}%")
-		print(f"     4×:                          {srt_data_rex_4x_cnt:>26} {to_percent(srt_data_rex_4x_cnt, data_pkts_org_received_lost):>8}%")
-		print(f"     more:                        {srt_data_rex_5x_more_cnt:>26} {to_percent(srt_data_rex_5x_more_cnt, data_pkts_org_received_lost):>8}%")
+		print(f"     once:                        {rex_once_cnt:>26} {to_percent(rex_once_cnt, data_pkts_org_received_lost):>8}%")
+		print(f"     twice:                       {rex_twice_cnt:>26} {to_percent(rex_twice_cnt, data_pkts_org_received_lost):>8}%")
+		print(f"     3×:                          {rex_3x_cnt:>26} {to_percent(rex_3x_cnt, data_pkts_org_received_lost):>8}%")
+		print(f"     4×:                          {rex_4x_cnt:>26} {to_percent(rex_4x_cnt, data_pkts_org_received_lost):>8}%")
+		print(f"     more:                        {rex_5x_more_cnt:>26} {to_percent(rex_5x_more_cnt, data_pkts_org_received_lost):>8}%")
+
+		return
 
 		print(" Traffic ".center(70, "~"))
 

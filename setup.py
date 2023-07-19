ac3d567199cbea09f1a5616fe8849fc1b923c69e
Maria Sharabayko <msharabayko@haivision.com>
2023-07-19 12:00:33 +0200

    Changed library version to v0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name='lib-tcpdump-processing',
-    version='0.1',
+    version='0.2',
     author='Maria Sharabayko',
     author_email='maria.bakholdina@gmail.com',
     packages=find_packages(),
